be581ceef9b9564286281e95b20dbe4a261de25b
Hale Deng <haledeng88@gmail.com>
2019-09-19 23:37:50 +0800

    feat: update v6 version

---
+++ src/v6/_example.js
@@ -0,0 +1,30 @@
+// 测试 koa2 的中间机制
+
+const App = require('./application')
+
+const app = new App()
+
+app.use(async (ctx, next) => {
+  console.log(1)
+  await next()
+  console.log(6)
+})
+
+app.use(async (ctx, next) => {
+  console.log(2)
+  await next()
+  console.log(5)
+})
+
+app.use(async (ctx, next) => {
+  console.log(3)
+  await next()
+  console.log(4)
+})
+
+app.use(async ctx => {
+  ctx.status = 201
+  ctx.body = 'hello world'
+})
+
+app.listen(3000, () => console.log('Server running on http://127.0.0.1:3000'))
---
+++ src/v6/context.js
@@ -0,0 +1,24 @@
+// 使用 Object.defineProperty 代理属性
+
+const proto = {}
+
+function proxyKey(property, key) {
+  Object.defineProperty(proto, key, {
+    configurable: true,
+    enumerable: true,
+    get() {
+      return this[property][key]
+    },
+    set(val) {
+      this[property][key] = val
+    }
+  })
+}
+
+const requestProperties = ['query']
+const responseProperties = ['body', 'status']
+
+requestProperties.forEach(key => proxyKey('request', key))
+responseProperties.forEach(key => proxyKey('response', key))
+
+module.exports = proto
